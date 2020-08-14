c282eb4c1e9e3557d1553f747ed31339eb71dbb2
KsenyBond <kseny.bondarchuk@gmail.com>
2020-08-14 12:51:15 +0300

    separate data from view

---
+++ src/mock/event.js
@@ -0,0 +1,131 @@
+import {getRandomInteger, generateValue} from "../utils.js";
+import {TRANSFER_TYPES, MINUTES_IN_DAY, MAX_SELECTED_OFFERS_NUMBER, MAX_AVAILABLE_OFFERS_NUMBER, DESTINATION_PHOTOS, MAX_DAYS_GAP} from "../const.js";
+
+const generateType = () => {
+  const types = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
+
+  return {
+    name: generateValue(types),
+    transfer: types.slice(0, TRANSFER_TYPES),
+    activity: types.slice(TRANSFER_TYPES),
+  };
+};
+
+const generateDestination = () => {
+  const destinations = [
+    `Rome`,
+    `Florence`,
+    `Bergamo`,
+    `Venice`
+  ];
+  const destinationRandomDescriptions = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    `Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus.`,
+    `In rutrum ac purus sit amet tempus.`
+  ];
+  const destinationDescription =
+    new Array(getRandomInteger(1, 5))
+      .fill()
+      .map(description => description = destinationRandomDescriptions[getRandomInteger(0, destinationRandomDescriptions.length - 1)]);
+  const uniqueDestinationDescription = new Set(destinationDescription);
+
+  return {
+    place: generateValue(destinations),
+    description: Array.from(uniqueDestinationDescription).join(` `),
+    photos: new Array(DESTINATION_PHOTOS).fill().map((photo) => `http://picsum.photos/248/152?r=${Math.random()}`),
+  };
+};
+
+const generateStartTime = () => {
+  const daysGap = getRandomInteger(-MAX_DAYS_GAP, MAX_DAYS_GAP);
+  const minutesGap = getRandomInteger(0, MINUTES_IN_DAY - 1);
+  const currentDate = new Date();
+
+  currentDate.setHours(0, 0, 0, 0);
+  currentDate.setMinutes(minutesGap);
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
+const generateEndTime = (start) => {
+  const currentDate = new Date(start.getTime());
+
+  currentDate.setHours(23, 59, 59, 999);
+
+  const minutesGap = getRandomInteger(0, currentDate.getTime() - start.getTime());
+
+  return new Date(start.getTime() + minutesGap);
+};
+
+const generateDuration = (start, end) => {
+  return end.getTime() - start.getTime();
+};
+
+const generateRandomOffer = () => {
+  const offers = [
+    {
+      type: `luggage`,
+      name: `Add luggage`,
+      price: 50
+    },
+    {
+      type: `meal`,
+      name: `Add meal`,
+      price: 15
+    },
+    {
+      type: `comfort`,
+      name: `Switch to comfort`,
+      price: 80
+    },
+    {
+      type: `seats`,
+      name: `Choose seats`,
+      price: 5
+    },
+    {
+      type: `train`,
+      name: `Travel by train`,
+      price: 40
+    }
+  ];
+
+  return generateValue(offers);
+};
+
+const generateOffers = (selectedCount, availableCount) => {
+  return {
+    selected: new Array(getRandomInteger(0, selectedCount)).fill().map(generateRandomOffer),
+    available: new Array(getRandomInteger(0, availableCount)).fill().map(generateRandomOffer),
+  };
+};
+
+const generateEvent = () => {
+  const type = generateType();
+  const destination = generateDestination();
+  const startTime = generateStartTime();
+  const endTime = generateEndTime(startTime);
+  const duration = generateDuration(startTime, endTime);
+  const price = getRandomInteger(10, 250);
+  const offers = generateOffers(MAX_SELECTED_OFFERS_NUMBER, MAX_AVAILABLE_OFFERS_NUMBER);
+
+  return {
+    type,
+    destination,
+    startTime,
+    endTime,
+    duration,
+    price,
+    offers,
+  };
+};
+
+export {generateEvent};
--- src/view/event.js
+++ src/view/event.js
@@ -1,32 +1,70 @@
-const createEventTemplate = () => {
+const generateDatetime = (time) => {
+  time = time.toLocaleString('en-GB');
+
+  return `${time.slice(0, 10).split(`/`).reverse().join(`-`)}T${time.slice(12, 17)}`;
+};
+
+const generateDurationDHM = (milliseconds) => {
+  let days = Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+
+  days = days !== 0 ? `${days}D` : ``;
+  hours = hours !== 0 ? `${hours}H` : ``;
+  minutes = minutes !== 0 ? `${minutes}M` : ``;
+
+  return `${days} ${hours} ${minutes}`.trim();
+};
+
+const createEventSelectedOffersTemplate = (offers) => {
+  const selectedOffers = offers.map((offer) =>
+    `<li class="event__offer">
+      <span class="event__offer-title">${offer.name}</span>
+      &plus;
+      &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
+    </li>`);
+  const selectedUniqueOffers = new Set(selectedOffers);
+
+  return Array.from(selectedUniqueOffers).join(``);
+};
+
+const createEventTemplate = (event) => {
+  const {type, destination, startTime, endTime, duration, price, offers} = event;
+
+  const typeName = type.name;
+  const preposition = type.transfer.includes(typeName) ? `to` : `in`;
+  const iconType = typeName.toLowerCase();
+  const start = startTime.toLocaleTimeString('en-GB').slice(0, -3);
+  const end = endTime.toLocaleTimeString('en-GB').slice(0, -3);
+  const datetimeStart = generateDatetime(startTime);
+  const datetimeEnd = generateDatetime(endTime);
+  const durationDHM = generateDurationDHM(duration);
+  const selectedOffersTemplate = createEventSelectedOffersTemplate(offers.selected);
+
   return (
     `<li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${iconType}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi to Amsterdam</h3>
+        <h3 class="event__title">${typeName} ${preposition} ${destination.place}</h3>
 
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime=${datetimeStart}>${start}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime=${datetimeEnd}>${end}</time>
           </p>
-          <p class="event__duration">30M</p>
+          <p class="event__duration">${durationDHM}</p>
         </div>
 
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
         </p>
 
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">20</span>
-           </li>
+          ${selectedOffersTemplate}
         </ul>
 
         <button class="event__rollup-btn" type="button">
